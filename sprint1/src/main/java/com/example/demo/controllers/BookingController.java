6158711719a8465e0d64f54112bfb7211e15c3ae
amishaphabba <phabbaamisha@gmail.com>
2022-04-30 20:42:31 +0530

    NEW Change

--- sprint1/src/main/java/com/example/demo/controllers/BookingController.java
+++ sprint1/src/main/java/com/example/demo/controllers/BookingController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entities.Booking;
 import com.example.demo.services.BookingService;
-
+//
 @RestController
 public class BookingController {
 	@Autowired
